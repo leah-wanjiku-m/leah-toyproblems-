a1115136e0ffad17ed2c35b6ce9715ae2601a45a
leah wanjiku <leagm22@gmail.com>
2022-12-01 09:29:04 +0300

    Completed assignment

---
+++ netcalculator/README.md
@@ -0,0 +1,2 @@
+project name net calculator
+it calculates the customers payee deductions when he inputs his grosssalary
--- netcalculator/index.js
+++ netcalculator/index.js
@@ -1,46 +1,60 @@
 const prompt=require("prompt-sync")();
-let user=prompt("Enter gross salary:")
-function tax(grossSalary){
-    var nhif= nhifDeduction;
-    var nssf=nssfDeduction;
-    var paye=payecheck;
-    var netpay=grossSalary-paye-nssf-nhif
-    console.log(netsalary);
+let user=prompt("Enter gross salary:" )
+let pay=user;
+let paye;
+function tax() {
+const payeNew = payeCheck(pay);
+  const nssf = nssfDeduction(pay);
+  const nhif = nhifDeduction(pay);
+  const netSalary = pay- payeNew - nssf - nhif;
+  console.log(netSalary);
 }
-function payecheck (grossSalary) {
-if (grossSalary<=24000){
-    console.log(grossSalary *0.1);
+
+function payeCheck (pay) {
+    if (pay >= 0 && pay <= 24000) {
+        paye = 0.1 * pay;
+        return paye;
+      } else if (pay > 24000 && pay <= 32333) {
+        paye = 0.25 * pay;
+        return paye;
+      } else if (pay > 32333) {
+        paye = 0.3 * pay;
+        return paye;
+      }
+    
+    console.log(pay)
 }
-else if(grossSalary>24001){
-    console.log(grossSalary*0.25);
-}
-else if(grossSalary>32333){
-    console.log(grossSalary*0.3);
-}
-}
-function nhifDeduction(grossSalary){
-    if(grossSalary<6000) {
-        return 150;
-    }else if(grossSalary<8000){
-        return 300
-    }else if(grossSalary<12000){
-        return 400
-    }else if (grossSalary<15000){
-        return 500
-    }else if (grossSalary<20000){
-        return 600
-    }else if (grossSalary<25000){
-        return 700
-    }else if (grossSalary<30000){
-        return 850
-    }else if (grossSalary>35000){
-        return 900
-    }else{
-        return 950
+function nhifDeduction(pay) {
+    if (pay >= 0 && pay <= 5999) {
+      return 150;
+    } else if (pay >= 6000 && pay <= 7999) {
+      return 300;
+    } else if (pay >= 8000 && pay<= 11999) {
+      return 400;
+    } else if (pay >= 12000 && pay <= 14999) {
+      return 500;
+    } else if (pay >= 15000 && pay<= 19999) {
+      return 600;
+    } else if (pay >= 20000 && pay <= 24999) {
+      return 750;
+    } else if (pay >= 25000 && pay <= 29999) {
+      return 850;
+    } else if (pay >= 30000 && pay <=35000){
+        return 900;
+    } else if (pay >= 35000 && pay <=39000){
+        return 950;
+    } else if (pay >= 40000 && pay <=44999){
+        return 1000;
+    } else if (pay >= 45000 && pay <=49999){
+        return 900;
     }
 
 }
-let nssf=400;
-totaldeductions= nhif+paye+nssf;
-netsalary=grossSalary-totaldeductions;
-console.log(`grossSalary is${grossSalary}`);
+function nssfDeduction(pay) {
+    pay = pay * 0.06;
+    return pay;
+  }
+    tax();
+
+
+
